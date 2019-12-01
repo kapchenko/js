cd05d1516319b9b73db288923cb2132ffd42c679
kapchenko <33328936+kapchenko@users.noreply.github.com>
2019-12-02 00:36:37 +0200

    done

---
+++ homework_1.js
@@ -0,0 +1,18 @@
+// Exercise. 
+// Need to change the order of result variable "a" and "b" using only one additional variable
+let a = 'green';
+let b = 'red';
+
+// Your code
+
+let c = 2 ;
+if ( c<1){
+    a = b;
+console.log(a); // Should equal 'red'
+}
+else {
+    b=a ; 
+    console.log(b); // Should equal 'green'
+}
+
+
